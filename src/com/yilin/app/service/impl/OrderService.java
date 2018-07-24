eb645e546cd0e33b252f7db33e4fa8c778b097ac
cc <784264581@qq.com>
2018-07-24 23:18:47 +0800

    接口实现

--- src/com/yilin/app/service/impl/OrderService.java
+++ src/com/yilin/app/service/impl/OrderService.java
@@ -4,9 +4,11 @@ import com.yilin.app.common.Page;
 import com.yilin.app.domain.Orders;
 import com.yilin.app.mapper.OrdersMapper;
 import com.yilin.app.service.IOrderService;
+import com.yilin.app.utils.OrderNumberBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,34 +23,43 @@ public class OrderService implements IOrderService {
     OrdersMapper ordersMapper;
 
     @Override
-    public Page selectPage(Integer userId, int pageIndex, int pageSize) throws Exception {
+    public Page selectPage(Integer userId, int start, int pageSize) throws Exception {
         Map<String, Object> map = new HashMap<>();
         map.put("user_id", userId);
-        map.put("index", (pageIndex - 1) * pageSize);
+        map.put("index", (start - 1) * pageSize);
         map.put("pageSize", pageSize);
         List<Orders> list = ordersMapper.selectAll(map);
-        Page page = new Page(pageSize, pageIndex, list.size(), list);
+        Page page = new Page(pageSize, start, list.size(), list);
         return page;
     }
 
     @Override
-    public void updateStatus(int id, int status) throws Exception {
-
+    public void updateStatus(int id, int userId, int status) throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        map.put("user_id", userId);
+        map.put("status", status);
+        map.put("id", id);
+        ordersMapper.updateStatus(map);
     }
 
     @Override
     public Orders findOrder(int id) throws Exception {
-        return null;
+        return ordersMapper.selectByPrimaryKey(id);
     }
 
     @Override
     public void createOrder(Orders orders) throws Exception {
-
+        orders.setTime(new Date());
+        orders.setIdentifier(OrderNumberBuilder.getOrderIdByUUId());
+        ordersMapper.insert(orders);
     }
 
     @Override
-    public int getCount(int userId, Integer status) throws Exception {
-        return 0;
+    public int getCount(Integer userId, Integer status) throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        map.put("user_id", userId);
+        map.put("status", status);
+        return ordersMapper.count(map);
     }
 
     @Override
@@ -58,6 +69,9 @@ public class OrderService implements IOrderService {
 
     @Override
     public void removeOrder(int id, int userId) throws Exception {
-
+        Map<String, Object> map = new HashMap<>();
+        map.put("user_id", userId);
+        map.put("id", id);
+        ordersMapper.deleteById(map);
     }
 }
