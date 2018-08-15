6d0ce49a15573c4b70d02dc5e291002b7a32197f
784264581@qq.com <784264581@qq.com>
2018-08-15 12:13:21 +0800

    功能修改

--- src/com/yilin/system/domain/Poster.java
+++
@@ -1,9 +0,0 @@
-package com.yilin.system.domain;
-
-/**
- * Created by cc on 2018/8/7.
- */
-public class Poster {
-
-    int id;
-}
--- src/com/yilin/system/mapper/PosterMapper.java
+++
@@ -1,7 +0,0 @@
-package com.yilin.system.mapper;
-
-/**
- * Created by cc on 2018/8/7.
- */
-public interface PosterMapper {
-}
